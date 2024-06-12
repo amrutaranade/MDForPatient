773c2f5b6c9b2f41fc8a97585dab8f8df94c3e4a
Taiyabba Shaikh <tshaikh@ocgcomapanies.com>
2024-06-12 11:16:22 +0530

    Worked on seeding duplications

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,19 +1,35 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
+use App\Models\Seed;
+use Database\Seeders\ImportCountriesSeeder;
+use Database\Seeders\ImportStatesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    protected $seedFiles = [
+        ImportCountriesSeeder::class,
+        ImportStatesSeeder::class,
+    ];
+
     public function run()
     {
-        $this->call(ImportCountriesSeeder::class);
-        $this->call(ImportStatesSeeder::class);
+        foreach ($this->seedFiles as $seedFile) {
+            // check if this seed exists in table or not
+            $exists = Seed::whereName($seedFile)->first();
+
+            if ($exists) {
+                $this->command->info("Seed already executed, skipping file: $seedFile");
+                continue;
+            }
+
+            // execute seed
+            $this->call($seedFile);
+
+            // generate record of execution
+            Seed::create([
+                'name' => $seedFile,
+            ]);
+        }
     }
 }
