ec49fe1e43513cc336bf1fcf487ba483cfedb08f
Taiyabba Shaikh <tshaikh@ocgcomapanies.com>
2024-06-12 17:51:54 +0530

    welcome email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -9,6 +9,7 @@ use Google\Client as GoogleClient;
 use Google\Service\Gmail\Message;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\OtpEmail;
+use App\Mail\WelcomeEmail;
 class EmailController extends Controller
 {
     
@@ -16,6 +17,11 @@ class EmailController extends Controller
         public function sendEmail($recipientEmail, $details)
         {
             Mail::to($recipientEmail)->send(new OtpEmail($details));
+        }
+
+        public function sendWelcomEmail($recipientEmail, $details)
+        {
+            Mail::to($recipientEmail)->send(new WelcomeEmail($details));
         }
     
 
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $details['title'] }}</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            background-color: #ffffff;
+            margin: 50px auto;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+        }
+        .header {
+            text-align: center;
+            padding-bottom: 20px;
+            border-bottom: 1px solid #dddddd;
+        }
+        .content {
+            padding: 20px 0;
+        }
+        .footer {
+            text-align: center;
+            padding-top: 20px;
+            border-top: 1px solid #dddddd;
+            color: #888888;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>{{ $details['title'] }}</h2>
+        </div>
+        <div class="content">
+            <p>Dear Recipient.</p>
+            <p>Greetings from MD For Patients!</p>
+            <p>We are sending you this email to provide you with your consultation number. Please use the same to check your consultation</p>
+            <p><strong>Consultation Number  : {{ $details['body'] }}</strong></p>
+            <p><i>This is an automatically generated email.</i></p>
+            <p>Client Services<br>MD for Patients</p>
+        </div>
+
+    </div>
+</body>
+</html>
